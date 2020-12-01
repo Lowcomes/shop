3573138304281445aef692b561a91aa515a26c45
Lowcomes <75334804+Lowcomes@users.noreply.github.com>
2020-12-01 23:01:17 +0300

    Shop

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.product').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+        variableWidth: true,
+        centerMode:true,
+        arrows: false,
+      });
+      $('.productlist_slider').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+        variableWidth: true,
+        centerMode:true,
+        arrows: false,
+      });  
+});
